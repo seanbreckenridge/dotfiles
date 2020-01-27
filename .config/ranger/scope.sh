d61ad0aee407ed2576d0dd610caf679b5db1fa1e
Sean Breckenridge <seanbrecke@gmail.com>
2020-01-27 00:43:04 -0800

    ranger: add preview script

---
+++ .config/ranger/scope.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+# ranger supports enhanced previews.  If the option "use_preview_script"
+# is set to True and this file exists, this script will be called and its
+# output is displayed in ranger.  ANSI color codes are supported.
+
+# NOTES: This script is considered a configuration file.  If you upgrade
+# ranger, it will be left untouched. (You must update it yourself.)
+# Also, ranger disables STDIN here, so interactive scripts won't work properly
+
+# Meanings of exit codes:
+# code | meaning    | action of ranger
+# -----+------------+-------------------------------------------
+# 0    | success    | success. display stdout as preview
+# 1    | no preview | failure. display no preview at all
+# 2    | plain text | display the plain content of the file
+# 3    | fix width  | success. Don't reload when width changes
+# 4    | fix height | success. Don't reload when height changes
+# 5    | fix both   | success. Don't ever reload
+# 6    | image      | success. display the image $cached points to as an image preview
+# 7    | image      | success. display the file directly as an image
+
+# Meaningful aliases for arguments:
+path="$1"            # Full path of the selected file
+#width="$2"           # Width of the preview pane (number of fitting characters)
+#height="$3"          # Height of the preview pane (number of fitting characters)
+cached="$4"          # Path that should be used to cache image previews
+preview_images="$5"  # "True" if image previews are enabled, "False" otherwise.
+
+maxln=100    # Stop after $maxln lines.  Can be used like ls | head -n $maxln
+
+# Find out something about the file:
+mimetype=$(file --mime-type -Lb "$path")
+extension=$(/bin/echo "${path##*.}" | awk '{print tolower($0)}')
+
+# Functions:
+# runs a command and saves its output into $output.  Useful if you need
+# the return value AND want to use the output in a pipe
+try() { output=$(eval '"$@"'); }
+
+# writes the output of the previously used "try" command
+dump() { /bin/echo "$output"; }
+
+# a common post-processing function used after most commands
+trim() { head -n "$maxln"; }
+
+# wraps highlight to treat exit code 141 (killed by SIGPIPE) as success
+safepipe() { "$@"; test $? = 0 -o $? = 141; }
+
+# Image previews, if enabled in ranger.
+if [ "$preview_images" = "True" ]; then
+    case "$mimetype" in
+        image/svg+xml)
+           convert "$path" "$cached" && exit 6 || exit 1;;
+        image/*)
+            exit 7;;
+        # Image preview for video, disabled by default.:
+        ###video/*)
+        ###    ffmpegthumbnailer -i "$path" -o "$cached" -s 0 && exit 6 || exit 1;;
+    esac
+fi
+
+case "$extension" in
+    # PDF documents:
+    pdf)
+	    try pdftoppm -jpeg -singlefile "$path" "${cached//.jpg}" && exit 6 || exit 1;;
+    json)
+      head -n "$maxln" "$path"; exit 0;;
+esac
+
+case "$mimetype" in
+    # Display JSON
+    application/json)
+      head -n "$maxln" "$path"; exit 0;;
+    # Display information about media files:
+    video/* | audio/*)
+        exiftool "$path" && exit 5
+        # Use sed to remove spaces so the output fits into the narrow window
+        try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; } || exit 1;;
+    # Syntax highlight for text files:
+    text/* | */xml)
+        if [ "$(tput colors)" -ge 256 ]; then
+            pygmentize_format=terminal256
+            highlight_format=ansi
+        else
+            pygmentize_format=terminal
+            highlight_format=ansi
+        fi
+        try safepipe pistol --out-format=${highlight_format} "$path" && { dump | trim; exit 5; }
+        ;;
+esac
+
+# defualt if extension/mimetype both fail
+try safepipe pistol "$path" && { dump | trim; exit 5;}
+
+exit 1
+
