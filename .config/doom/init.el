b6e4539b7ca7dc2ba717f4db7d713b06f3f5306e
Sean Breckenridge <seanbrecke@gmail.com>
2020-02-26 12:08:13 -0800

    use lsp server for kotlin

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -129,7 +129,7 @@
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        (javascript +lsp)   ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
-       kotlin              ; a better, slicker Java(Script)
+       (kotlin +lsp)       ; a better, slicker Java(Script)
        latex               ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
