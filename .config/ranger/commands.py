413c7aaa1495b81ba0bf83b5c39dcfb857f011f9
Sean Breckenridge <seanbrecke@gmail.com>
2020-03-31 19:07:46 -0700

    clean up ranger functions
    don't use fzf-bring/archive related functions
    I just use shell commands in rc.conf instead
    
    use fd instead instead of find
    remove quantifier and use a separate command
    to show hidden files

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -1,41 +1,32 @@
-# Modified from https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.config/ranger/commands.py
-#
-# This is a sample commands.py.  You can add your own commands here.
-#
-# Please refer to commands_full.py for all the default commands and a complete
-# documentation.  Do NOT add them all here, or you may end up with defunct
-# commands when upgrading ranger.
 
-# You always need to import ranger.api.commands here to get the Command class:
 import os
+import subprocess
 from ranger.api.commands import Command
-from ranger.core.loader import CommandLoader
 
-# https://github.com/ranger/ranger/wiki/Integrating-File-Search-with-fzf
-# Now, simply bind this function to a key, by adding this to your ~/.config/ranger/rc.conf: map <C-f> fzf_select
+
 class fzf_select(Command):
     """
     :fzf_select
 
     Find a file using fzf.
-
     With a prefix argument select only directories.
 
-    See: https://github.com/junegunn/fzf
+    Use fd instead of find
+
+    Modified from: https://github.com/junegunn/fzf
     """
 
-    def execute(self):
-        import subprocess
+    def find_command(self):
+        """To support nicer polymorphism"""
+        return "fd -L"
 
-        if self.quantifier:
-            # match only directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-        else:
-            # match files and directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m --preview='${HOME}/.config/fzf_preview {}'"
-        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+    @property
+    def command(self):
+        """Command to run to run the find/fzf command"""
+        return "{} | fzf +m".format(self.find_command())
+
+    def execute(self):
+        fzf = self.fm.execute_command(self.command, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.decode("utf-8").rstrip("\n"))
@@ -45,100 +36,10 @@ class fzf_select(Command):
                 self.fm.select_file(fzf_file)
 
 
-class fzf_bring(Command):
+class fzf_select_hidden(fzf_select):
     """
-    :fzf_bring
-
-    Find a file using fzf and bring it to the current directory.
-
-    See: https://github.com/junegunn/fzf
+    same as fzf_select but shows hidden files
     """
 
-    def execute(self):
-        import subprocess
-        import shutil
-
-        if self.quantifier:
-            # match only directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m --preview='${HOME}/.config/fzf_preview {}'"
-        else:
-            # match files and directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
-        stdout, stderr = fzf.communicate()
-        if fzf.returncode == 0:
-            fzf_path = os.path.abspath(stdout.decode("utf-8").rstrip("\n"))
-            shutil.move(fzf_path, self.fm.thisdir.path)
-
-
-class compress(Command):
-    def execute(self):
-        """ Compress marked files to current directory """
-        cwd = self.fm.thisdir
-        marked_files = cwd.get_selection()
-
-        if not marked_files:
-            return
-
-        def refresh(_):
-            cwd = self.fm.get_directory(original_path)
-            cwd.load_content()
-
-        original_path = cwd.path
-        parts = self.line.split()
-        au_flags = parts[1:]
-
-        descr = "compressing files in: " + os.path.basename(parts[1])
-        obj = CommandLoader(
-            args=["apack"]
-            + au_flags
-            + [os.path.relpath(f.path, cwd.path) for f in marked_files],
-            descr=descr,
-        )
-
-        obj.signal_bind("after", refresh)
-        self.fm.loader.add(obj)
-
-    def tab(self):
-        """ Complete with current folder name """
-
-        extension = [".zip", ".tar.gz", ".rar", ".7z"]
-        return [
-            "compress " + os.path.basename(self.fm.thisdir.path) + ext
-            for ext in extension
-        ]
-
-
-class extracthere(Command):
-    def execute(self):
-        """ Extract copied files to current directory """
-        copied_files = tuple(self.fm.copy_buffer)
-
-        if not copied_files:
-            return
-
-        def refresh(_):
-            cwd = self.fm.get_directory(original_path)
-            cwd.load_content()
-
-        one_file = copied_files[0]
-        cwd = self.fm.thisdir
-        original_path = cwd.path
-        au_flags = ["-X", cwd.path]
-        au_flags += self.line.split()[1:]
-        au_flags += ["-e"]
-
-        self.fm.copy_buffer.clear()
-        self.fm.cut_buffer = False
-        if len(copied_files) == 1:
-            descr = "extracting: " + os.path.basename(one_file.path)
-        else:
-            descr = "extracting files from: " + os.path.basename(one_file.dirname)
-        obj = CommandLoader(
-            args=["aunpack"] + au_flags + [f.path for f in copied_files], descr=descr
-        )
-
-        obj.signal_bind("after", refresh)
-        self.fm.loader.add(obj)
+    def find_command(self):
+        return "fd -L -H"
