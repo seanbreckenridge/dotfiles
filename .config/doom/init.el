91cce32f63644d31eea8c8e56106d48b2da660c2
Sean Breckenridge <seanbrecke@gmail.com>
2020-04-06 09:49:10 -0700

    add common lisp packages

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -111,7 +111,7 @@
        ;;assembly          ; assembly for fun or debugging
        (cc +lsp)           ; C/C++/Obj-C madness
        clojure             ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       common-lisp         ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
