95d74c35a2f74336ed40014c1759309012780fbc
Sean Breckenridge <seanbrecke@gmail.com>
2020-04-05 21:35:52 -0700

    update package lists

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -130,7 +130,7 @@
        ;;idris             ;
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        (javascript +lsp)   ; all(hope(abandon(ye(who(enter(here))))))
-       ;;julia             ; a better, faster MATLAB
+       julia               ; a better, faster MATLAB
        (kotlin +lsp)       ; a better, slicker Java(Script)
        latex               ; writing papers in Emacs has never been so fun
        ;;lean
