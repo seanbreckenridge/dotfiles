115d2721c18f3aa0988d6f9689fcd26b8b68c1b1
Sean Breckenridge <seanbrecke@gmail.com>
2020-04-17 05:40:59 -0700

    update doom package list w/ new undo item

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -70,6 +70,7 @@
        (dired +ranger)     ; making dired pretty [functional]
        electric            ; smarter, keyword-based electric-indent
        ibuffer             ; interactive buffer management
+       undo                ; persistent, smarter undo for your inevitable mistakes
        vc                  ; version-control and Emacs, sitting in a tree
 
        :term
