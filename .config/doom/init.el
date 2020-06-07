9cfa13fc7b8640df1ab8f44ca8b4438330300a8f
Sean Breckenridge <seanbrecke@gmail.com>
2020-06-06 21:38:58 -0700

    perl -> raku

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -149,7 +149,7 @@
         ;;+pandoc          ; export-with-pandoc support
         ;;+pomodoro        ; be fruitful with the tomato technique
         +present)          ; using org-mode for presentations
-       perl                ; write code no one else can comprehend
+       raku                ; write code no one else can comprehend (previously perl)
        php                 ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
