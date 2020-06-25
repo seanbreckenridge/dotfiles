9a7d59286a43863a40199c68206b1c0dbbf82e0d
Sean Breckenridge <seanbrecke@gmail.com>
2020-06-24 17:15:49 -0700

    add some completion features form gotbletu

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -7,6 +7,17 @@ zmodload zsh/complist  # http://zsh.sourceforge.net/Doc/Release/Zsh-Modules.html
 zstyle ':completion:*' menu select  #  http://zsh.sourceforge.net/Guide/zshguide06.html
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*' # Auto complete with case insenstivity
 setopt globdots # allow autocompletion to target hidden files
+setopt glob_complete  # wildcard completion, e.g. *.png
+setopt completealiases # expand aliases before completing
+
+# from gotbletu
+# https://github.com/gotbletu/dotfiles_v2/blob/master/normal_user/zshrc/.zshrc
+# better killall completion
+zstyle ':completion:*:killall:*' command 'ps -u $USER -o cmd'
+# when new programs is installed, auto update autocomplete without reloading shell
+zstyle ':completion:*' rehash true
+# Don't complete stuff already on the line
+zstyle ':completion::*:(rm|nvim|e|ls):*' ignore-line true
 
 # setup prompt
 parse_git_branch() {
