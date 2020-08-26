bc979ebcb51bbb10eb1bb686645dacd75a5f9d4e
Sean Breckenridge <seanbrecke@gmail.com>
2020-08-26 15:45:18 -0700

    add zsh history configuration

--- .config/my/my/config/__init__.py
+++ .config/my/my/config/__init__.py
@@ -6,5 +6,9 @@ from my.core import PathIsh, Paths
 
 class github:
     gdpr_dir: PathIsh = Path('~/data/github/gdpr').expanduser()
-    export_path: Paths = "~/data/github/ghexport/*.json"
+    export_path: Paths = "~/data/github/ghexport/"
     ghexport : Optional[PathIsh] = environ["REPOS"] + '/ghexport'
+
+class zsh:
+    export_path: Paths = "~/data/zsh_history/"
+    live_zsh_history: Optional[PathIsh] = environ["ZDOTDIR"] + '/.zsh_history'
