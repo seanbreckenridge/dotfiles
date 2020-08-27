cbc7dc5c17612cc65fdb522d93bd8d3ba2391817
Sean Breckenridge <seanbrecke@gmail.com>
2020-08-27 01:31:07 -0700

    HPI: add commits configuration

--- .config/my/my/config/__init__.py
+++ .config/my/my/config/__init__.py
@@ -1,5 +1,10 @@
+"""
+Configuration file for https://github.com/seanbreckenridge/HPI/
+[Human Programming Interface]
+"""
+
 from os import environ, path
-from typing import Optional
+from typing import Optional, List
 from pathlib import Path
 
 from my.core import PathIsh, Paths
@@ -19,13 +24,28 @@ class reddit:
     export_path: Paths = "~/data/rexport/"
     rexport: Optional[PathIsh] = repo("rexport")
 
+
 class weight:
     logfile: PathIsh = "~/data/weight.log"
+
 
 class zsh:
     export_path: Paths = "~/data/zsh_history/"
     live_zsh_history: Optional[PathIsh] = path.join(environ["ZDOTDIR"], ".zsh_history")
 
-#class stackexchange:
+
+class commits:
+    # names: List[str] = ["Sean Breckenridge"]
+    emails: List[str] = [
+        "seanbrecke@gmail.com",
+        "sbrecken@ucsc.edu",
+        "purplepinapplesyt@gmail.com",
+    ]
+    roots: Paths = [
+        Path(environ["REPOS"]),
+    ]
+
+
+# class stackexchange:
 #    export_path: Paths = "~/data/stexport"
 #    stexport: Optional[PathIsh] = repo("stexport")
