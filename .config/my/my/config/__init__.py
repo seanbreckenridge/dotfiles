7f879d8d67119fd18abe46d7ff8ce06abaed59d4
Sean Breckenridge <seanbrecke@gmail.com>
2020-08-26 19:44:19 -0700

    add weight to hpi config

--- .config/my/my/config/__init__.py
+++ .config/my/my/config/__init__.py
@@ -1,14 +1,31 @@
-from os import environ
+from os import environ, path
 from typing import Optional
 from pathlib import Path
 
 from my.core import PathIsh, Paths
 
+
+def repo(repo_name: str) -> str:
+    return path.join(environ["REPOS"], repo_name)
+
+
 class github:
-    gdpr_dir: PathIsh = Path('~/data/github/gdpr').expanduser()
+    gdpr_dir: PathIsh = Path("~/data/github/gdpr").expanduser()
     export_path: Paths = "~/data/github/ghexport/"
-    ghexport : Optional[PathIsh] = environ["REPOS"] + '/ghexport'
+    ghexport: Optional[PathIsh] = repo("ghexport")
+
+
+class reddit:
+    export_path: Paths = "~/data/rexport/"
+    rexport: Optional[PathIsh] = repo("rexport")
+
+class weight:
+    logfile: PathIsh = "~/data/weight.log"
 
 class zsh:
     export_path: Paths = "~/data/zsh_history/"
-    live_zsh_history: Optional[PathIsh] = environ["ZDOTDIR"] + '/.zsh_history'
+    live_zsh_history: Optional[PathIsh] = environ["ZDOTDIR"] + "/.zsh_history"
+
+#class stackexchange:
+#    export_path: Paths = "~/data/stexport"
+#    stexport: Optional[PathIsh] = repo("stexport")
