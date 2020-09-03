36d04b174c12c2e7eaa48115b1a6b70db4e7fb4b
Sean Breckenridge <seanbrecke@gmail.com>
2020-09-03 15:25:12 -0700

    ipython load common python modules on startup

---
+++ .local/share/ipython/profile_default/startup/00-load-modules.py
@@ -0,0 +1,12 @@
+# load modules that I use in testing things automatically
+import time
+import os
+import sys
+import json
+import tempfile
+from pathlib import Path
+from datetime import datetime, timedelta, timezone
+from collections import Counter
+from glob import glob
+from pprint import pprint
+
