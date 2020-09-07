9032546b2a3c4f968ab808f74c740c17e31a36bf
Sean Breckenridge <seanbrecke@gmail.com>
2020-09-06 19:58:42 -0700

    HPI: describe sources

--- .config/my/my/config/__init__.py
+++ .config/my/my/config/__init__.py
@@ -13,31 +13,39 @@ from my.core import PathIsh, Paths
 def repo(repo_name: str) -> str:
     return path.join(environ["REPOS"], repo_name)
 
-
+# combines:
+# periodic exports from: https://github.com/karlicoss/ghexport
+# github GDPR export
 class github:
     gdpr_dir: PathIsh = "~/data/github/gdpr"
     export_path: Paths = "~/data/github/ghexport/"
     ghexport: Optional[PathIsh] = repo("ghexport")
 
 
+# combines:
+# periodic exports from: https://github.com/karlicoss/rexport/
+# comment export from: https://github.com/seanbreckenridge/pushshift_comment_export
 class reddit:
     export_path: Paths = "~/data/rexport/"
     pushshift_export_path: Paths = "~/data/rps_comments/*.json"
     rexport: Optional[PathIsh] = repo("rexport")
 
-
+# parses a manually created log file with https://github.com/seanbreckenridge/autotui
+# https://github.com/seanbreckenridge/HPI/blob/master/my/body/weight.py
 class weight:
     datafile: PathIsh = "~/data/weight.json"
 
-
+# parses my zsh history and any backups
 class zsh:
     export_path: Paths = "~/data/zsh_history/"
     live_file: Optional[PathIsh] = path.join(environ["ZDOTDIR"], ".zsh_history")
 
+# parses current/finished http://todotxt.org/ using topydo
 class todotxt:
     export_path: Paths = "~/data/todotxt/"
     live_file: Optional[PathIsh] = path.join(environ["XDG_CONFIG_HOME"], "todo", "todo.txt")
 
+# parses information from git repositories which match my emails
 class commits:
     # names: List[str] = ["Sean Breckenridge"]
     emails: List[str] = [
@@ -69,22 +77,27 @@ class league_of_legends:
 class steam:
     export_path: Paths = "~/data/steam.json"
 
-# uses https://github.com/seanbreckenridge/steamscraper
+# https://github.com/seanbreckenridge/blizzard_gdpr_parser
 class blizzard:
     export_path: Paths = "~/data/blizzard/parsed.json"
 
+# https://github.com/seanbreckenridge/forum_parser
 class old_forums:
     export_path: Paths = "~/data/old_forums/*.json"
 
+# parses the GDPR export
 class skype:
     export_path: Paths = "~/data/skype.json"
 
+# parsers the GDPR export
 class facebook:
     gdpr_dir: PathIsh = "~/data/facebook_gdpr/"
 
+# parses backups of and the live ipython history sqlite databases
 class ipython:
     export_path: Paths = "~/data/ipython/*.sqlite"
 
+# parses https://takeout.google.com
 class google:
     takeout_path: Paths = "~/data/google_takeout/"
 
