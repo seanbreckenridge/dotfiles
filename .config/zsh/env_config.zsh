bb61914703f75fa39e2213be12cb9bfe99e7cee4
Sean Breckenridge <seanbrecke@gmail.com>
2020-10-18 01:51:28 -0700

    add up-live (ultimate plumber) alias

--- .config/zsh/env_config.zsh
+++ .config/zsh/env_config.zsh
@@ -99,6 +99,7 @@ alias wget='wget --hsts-file "${XDG_CACHE_HOME}/wget-hsts"'
 
 # Terminal encoding issues
 alias up="TERM=xterm up --no-colors"
+alias 'up-live=TERM=xterm up --no-colors --unsafe-full-throttle'
 
 # general environment variable configuration
 export YSU_MESSAGE_FORMAT="$(tput setaf 1)>>> %alias_type: %alias $(tput sgr0)"
