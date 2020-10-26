46933d807a7f9eeb85e410f9218948fec190a8a1
Sean Breckenridge <seanbrecke@gmail.com>
2020-10-25 20:58:31 -0700

    autoformat

--- .config/zsh/progressive_enhancement.zsh
+++ .config/zsh/progressive_enhancement.zsh
@@ -2,33 +2,35 @@
 # to a fancy version
 
 # if I type python with out any arguments, launch ipython instead
-python() { python3 "$@" }
+python() {
+	python3 "$@"
+}
 python3() {
-  if (( $# == 0 )); then
-    echo -e "$(tput setaf 2)Launching ipython instead...$(tput sgr0)"
-    ipython
-  else
-    "$(findbinary "python3")" "$@"
-  fi
+	if (($# == 0)); then
+		echo -e "$(tput setaf 2)Launching ipython instead...$(tput sgr0)"
+		ipython
+	else
+		"$(findbinary "python3")" "$@"
+	fi
 }
 
 # if I type cat with only one argument, which is a file that exists
 # and this is printing to STDOUT (not a pipe/file), use bat instead
 cat() {
-  if [[ -t 1 ]] && (($# == 1)) && [[ -r "$1" ]]; then
-    # https://github.com/sharkdp/bat
-    bat --pager=never "$1"
-  else
-    /bin/cat "$@"
-  fi
+	if [[ -t 1 ]] && (($# == 1)) && [[ -r "$1" ]]; then
+		# https://github.com/sharkdp/bat
+		bat --pager=never "$1"
+	else
+		/bin/cat "$@"
+	fi
 }
 
 # If I run 'ps -ef' with nothing else, without piping it into anything else, use procs instead
 ps() {
-  if [[ -t 1 ]] && [[ "$1" == "-ef" ]] && (($# == 1)); then
-    procs --pager=disable
-    echo "Instead of 'ps -ef | grep something', just use 'procs something'" 1>&2
-  else
-    /bin/ps "$@"
-  fi
+	if [[ -t 1 ]] && [[ "$1" == "-ef" ]] && (($# == 1)); then
+		procs --pager=disable
+		echo "Instead of 'ps -ef | grep something', just use 'procs something'" 1>&2
+	else
+		/bin/ps "$@"
+	fi
 }
