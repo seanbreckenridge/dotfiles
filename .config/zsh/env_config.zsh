e503c7ec9cb71823afb4d9e3f30862abec75bde2
Sean Breckenridge <seanbrecke@gmail.com>
2020-11-29 20:22:25 -0800

    mac updates

--- .config/zsh/env_config.zsh
+++ .config/zsh/env_config.zsh
@@ -127,7 +127,7 @@ if [[ -z "$ONLINUX" ]]; then
 	PATH="\
 /usr/local/bin:\
 ${HOME}/.rbenv/versions/2.7.1/bin:\
-${HOME}/Library/Python/3.8/bin:\
+${HOME}/Library/Python/3.9/bin:\
 /usr/local/opt/coreutils/libexec/gnubin:\
 /usr/local/opt/diffutils/bin:\
 /usr/local/opt/findutils/libexec/gnubin:\
