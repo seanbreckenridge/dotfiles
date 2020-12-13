44323a3411ee94cf96f11cc4a22dab602c741cc7
Sean Breckenridge <seanbrecke@gmail.com>
2020-12-12 16:32:38 -0800

    update aliases

--- .config/zsh/progressive_enhancement.zsh
+++ .config/zsh/progressive_enhancement.zsh
@@ -14,17 +14,6 @@ python3() {
 	fi
 }
 
-# if I type cat with only one argument, which is a file that exists
-# and this is printing to STDOUT (not a pipe/file), use bat instead
-cat() {
-	if [[ -t 1 ]] && (($# == 1)) && [[ -r "$1" ]]; then
-		# https://github.com/sharkdp/bat
-		bat --pager=never "$1"
-	else
-		command cat "$@"
-	fi
-}
-
 # If I run 'ps -ef' with nothing else, without piping it into anything else, use procs instead
 ps() {
 	if [[ -t 1 ]] && [[ "$1" == "-ef" ]] && (($# == 1)); then
