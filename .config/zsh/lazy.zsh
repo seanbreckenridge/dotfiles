81d2a439aef558450ee4d6d0ac8852e03a150184
Sean Breckenridge <seanbrecke@gmail.com>
2021-02-03 12:21:56 -0800

    packages: add nvm
    lazily load this when I first use it
    else NPM_CONFIG_PREFIX is set for
    global npm package usage
    
    when you first run nvm in some terminal
    it modifies the path so node and npm
    in that session point to the right
    things for doing development

--- .config/zsh/lazy.zsh
+++ .config/zsh/lazy.zsh
@@ -5,3 +5,8 @@ fuck() {
 	eval $(thefuck --alias) # redefines the function
 	fuck "$@"
 }
+
+nvm() {
+	NPM_CONFIG_PREFIX= source_if_exists /usr/share/nvm/init-nvm.sh && alias nvm='NPM_CONFIG_PREFIX= nvm'
+	nvm "$@"
+}
