1b95f9dc18426c4cb7e2d3a945fd207e776f955b
Sean Breckenridge <seanbrecke@gmail.com>
2021-05-07 03:23:42 -0700

    remove references to lightdm

--- .config/zsh/env_config.zsh
+++ .config/zsh/env_config.zsh
@@ -86,7 +86,7 @@ export JULIA_DEPOT_PATH="${XDG_DATA_HOME}/julia"
 export SHORTCUTS_DIR="${XDG_DATA_HOME}/shortcuts"
 
 # playlist manager: https://github.com/seanbreckenridge/plaintext-playlist
-export PLAINTEXT_PLAYLIST_MUSIC_DIR="${MUSIC}"
+export PLAINTEXT_PLAYLIST_MUSIC_DIR="${XDG_MUSIC_DIR}"
 export PLAINTEXT_PLAYLIST_PLAYLISTS="${XDG_DATA_HOME:-$HOME/.local/share}/plaintext_playlist"
 
 # https://github.com/seanbreckenridge/url_metadata
