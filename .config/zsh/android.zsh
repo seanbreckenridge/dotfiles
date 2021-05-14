b10cdeee2db7ba750c8f9345268ed7bac367f464
Sean Breckenridge <seanbrecke@gmail.com>
2021-05-13 22:50:45 -0700

    link to bgproc

--- .config/zsh/android.zsh
+++ .config/zsh/android.zsh
@@ -6,4 +6,5 @@ export TTT_HISTFILE="${HPIDATA}/phone/ttt/${SHELL_BOOT_TIME}.csv"
 
 # run bgproc jobs once a day when I login
 # https://github.com/seanbreckenridge/dotfiles/tree/master/.local/scripts/supervisor
+# https://github.com/seanbreckenridge/bgproc
 evry 1 day -run_android_jobs && "${HOME}/.local/scripts/supervisor/run_jobs" -o
