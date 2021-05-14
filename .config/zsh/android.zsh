9839ddc6e62655514e5437b895720bb57ce519c9
Sean Breckenridge <seanbrecke@gmail.com>
2021-05-13 22:42:42 -0700

    background jobs: run on android once a day

--- .config/zsh/android.zsh
+++ .config/zsh/android.zsh
@@ -3,3 +3,7 @@
 
 # https://github.com/seanbreckenridge/ttt
 export TTT_HISTFILE="${HPIDATA}/phone/ttt/${SHELL_BOOT_TIME}.csv"
+
+# run bgproc jobs once a day when I login
+# https://github.com/seanbreckenridge/dotfiles/tree/master/.local/scripts/supervisor
+evry 1 day -run_android_jobs && "${HOME}/.local/scripts/supervisor/run_jobs" -o
