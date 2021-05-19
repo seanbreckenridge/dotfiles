fbb748ba3335f2fcd818a0888c78b8449678b1ac
Sean Breckenridge <seanbrecke@gmail.com>
2021-05-18 22:56:49 -0700

    python: some pipenv compatability

--- .local/share/ipython/profile_default/startup/00-load-modules.py
+++ .local/share/ipython/profile_default/startup/00-load-modules.py
@@ -1,10 +1,12 @@
 # load modules that I use in testing things automatically
-import json
-import tempfile
-from pathlib import Path
-from datetime import datetime, timedelta, timezone
-from collections import Counter
-from glob import glob
-from pprint import pprint
-from itertools import chain
-from more_itertools import ilen, last
+import os
+if "PIPENV_ACTIVE" not in os.environ:
+    import json
+    import tempfile
+    from pathlib import Path
+    from datetime import datetime, timedelta, timezone
+    from collections import Counter
+    from glob import glob
+    from pprint import pprint
+    from itertools import chain
+    from more_itertools import ilen, last
