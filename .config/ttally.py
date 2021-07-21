ab4be078d9baf27afd134b71dfa39e95eb626eca
Sean Breckenridge <seanbrecke@gmail.com>
2021-07-21 15:28:46 -0700

    ttally updates

--- .config/ttally.py
+++ .config/ttally.py
@@ -11,12 +11,12 @@ class Weight(NamedTuple):
     pounds: float
 
 
-class Water(NamedTuple):
-    when: datetime
-    glasses: float
-
-
+# this also tracks water, either by attaching it
+# to the corresponding food, or by just adding
+# something with the text 'water'
 class Food(NamedTuple):
     when: datetime
     calories: int
     food: str
+    quantity: float
+    water: int  # ml
