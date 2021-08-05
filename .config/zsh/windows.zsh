d21103eaec7cab06a6ffbd8240ada14799e83ee5
Sean Breckenridge <seanbrecke@gmail.com>
2021-08-04 22:56:56 -0700

    run bgproc jobs on windows

--- .config/zsh/windows.zsh
+++ .config/zsh/windows.zsh
@@ -1,1 +1,6 @@
 #!/usr/bin/env zsh
+
+# run bgproc jobs once a day when I login
+# https://github.com/seanbreckenridge/dotfiles/tree/master/.local/scripts/supervisor
+# https://github.com/seanbreckenridge/bgproc
+evry 1 day -run_windows_jobs && "${HOME}/.local/scripts/supervisor/run_jobs" -o
