80462f3e16fbb69da9916121783c85fef6c979f8
Sean Breckenridge <seanbrecke@gmail.com>
2021-09-04 02:37:32 -0700

    add gpslogger rsync job

--- .config/zsh/android.zsh
+++ .config/zsh/android.zsh
@@ -1,7 +1,16 @@
 #!/usr/bin/env zsh
 # android-specific setup
+# this is mostly handling anacron-like tasks that run
 
 # run bgproc jobs
 # https://github.com/seanbreckenridge/dotfiles/tree/master/.local/scripts/supervisor
 # https://github.com/seanbreckenridge/bgproc
 evry 6 hours -run_android_jobs && "${HOME}/.local/scripts/supervisor/run_jobs" -o
+
+gpslogger_backup() {
+	su -c 'rsync -Pavh "/data/data/com.termux/files/home/shared/Android/data/com.mendhak.gpslogger/files/"* "/data/data/com.termux/files/home/storage/shared/data/gpslogger"'
+}
+
+# run this in the interactive shell since that works with su
+# otherwise calling it as a script behind bgproc fails
+evry 1 day -gpslogger && gpslogger_backup
