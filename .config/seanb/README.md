a4e774f44b49fa68b0130b26434566fa58ecff34
Sean Breckenridge <seanbrecke@gmail.com>
2021-11-15 14:53:00 -0800

    convert personal python package to non-namespace

--- .config/seanb/README.md
+++ .config/seanb/README.md
@@ -1,3 +1,3 @@
-This is installed as an editable package in `yadm bootstrap`
+This is installed as an package in `yadm bootstrap`
 
 Is essentially a package to store personal scripts/functions I want importable from anywhere
--- .config/seanb/setup.py
+++ .config/seanb/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_namespace_packages  # type: ignore[import]
+from setuptools import setup  # type: ignore[import]
 
 
 if __name__ == "__main__":
     setup(
         name="seanb",
         zip_safe=False,
-        packages=find_namespace_packages(".", include=("seanb.*",)),
+        packages=["seanb"],
         author="Sean Breckenridge",
         author_email="seanbrecke@gmail.com",
         python_requires=">=3.8",
