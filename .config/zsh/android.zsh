637216450ac63842e14a11b58982506fc8328309
Sean Breckenridge <seanbrecke@gmail.com>
2021-12-14 15:39:03 -0800

    create playlists on phone

--- .config/zsh/android.zsh
+++ .config/zsh/android.zsh
@@ -8,10 +8,5 @@
 # https://github.com/seanbreckenridge/bgproc
 evry 6 hours -run_android_jobs && "${HOME}/.local/scripts/supervisor/run_jobs" -o
 
-gpslogger_backup() {
-	su -c 'rsync -Pavh "/data/data/com.termux/files/home/shared/Android/data/com.mendhak.gpslogger/files/"* "/data/data/com.termux/files/home/storage/shared/data/gpslogger"'
-}
-
-# run this in the interactive shell since that works with su
-# otherwise calling it as a script behind bgproc fails
-evry 1 day -gpslogger && gpslogger_backup
+# for building rust/ffi python packages
+export CARGO_BUILD_TARGET=aarch64-linux-android
