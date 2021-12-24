161409c362796feecda8e9dc7a3b11f8a5514b07
Sean Breckenridge <seanbrecke@gmail.com>
2021-12-24 00:16:35 -0800

    android; syncfiles func

--- .config/zsh/android.zsh
+++ .config/zsh/android.zsh
@@ -8,5 +8,15 @@
 # https://github.com/seanbreckenridge/bgproc
 evry 6 hours -run_android_jobs && "${HOME}/.local/scripts/supervisor/run_jobs" -o
 
+# reset any periodic syncs and re-run bgproc jobs
+syncfiles() {
+	for tag in \
+		create_playlists \
+		backup_images; do
+		rm -vf "$(evry location -"$tag")"
+	done
+	run-jobs -o
+}
+
 # for building rust/ffi python packages
 export CARGO_BUILD_TARGET=aarch64-linux-android
