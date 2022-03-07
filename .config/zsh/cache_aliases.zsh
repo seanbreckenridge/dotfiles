362d202a22e8f4a128d9d6e535d7180c33b11873
Sean Breckenridge <seanbrecke@gmail.com>
2022-03-07 05:58:37 -0800

    alias cache; sort

--- .config/zsh/cache_aliases.zsh
+++ .config/zsh/cache_aliases.zsh
@@ -5,6 +5,6 @@
 hash evry && evry 1 week -zsh_alias_cache && {
 	ALIAS_CACHE_BACKUP_DIR="$(python3 -m my.utils.backup_to 'zsh_aliases')" || exit $?
 	ALIAS_CACHE_TARGET="${ALIAS_CACHE_BACKUP_DIR}/$(date +'%s')-$(uname)"
-	alias >"${ALIAS_CACHE_TARGET}"
+	alias | sort >"${ALIAS_CACHE_TARGET}"
 	unset ALIAS_CACHE_BACKUP_DIR ALIAS_CACHE_TARGET
 }
