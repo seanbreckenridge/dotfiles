2577271777cce2c6f997ff1c473a52bd6939f8e7
Sean Breckenridge <seanbrecke@gmail.com>
2022-03-07 04:49:06 -0800

    zsh alias dir: remove usless mkdir

--- .config/zsh/alias_cache.zsh
+++ .config/zsh/alias_cache.zsh
@@ -4,7 +4,6 @@
 
 hash evry && evry 1 week -zsh_alias_cache && {
 	ALIAS_CACHE_BACKUP_DIR="$(python3 -m my.utils.backup_to 'zsh_aliases')" || exit $?
-	mkdir -p "${ALIAS_CACHE_BACKUP_DIR}"
 	ALIAS_CACHE_TARGET="${ALIAS_CACHE_BACKUP_DIR}/$(date +'%s')-$(uname)"
 	alias >"${ALIAS_CACHE_TARGET}"
 	unset ALIAS_CACHE_BACKUP_DIR ALIAS_CACHE_TARGET
