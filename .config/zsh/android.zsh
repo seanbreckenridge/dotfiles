33a6fc4e76c460f9a9cbf73523528fa4da74982e
Sean Breckenridge <seanbrecke@gmail.com>
2022-03-23 16:17:34 -0700

    sync hpi config on termux

--- .config/zsh/android.zsh
+++ .config/zsh/android.zsh
@@ -7,6 +7,8 @@
 # https://github.com/seanbreckenridge/bgproc
 evry 6 hours -run_android_jobs && "${HOME}/.local/scripts/supervisor/run_jobs" -o
 
+evry 1 hour -sync_hpi_config && sync_hpi_config
+
 # reset any periodic syncs and re-run bgproc jobs
 syncfiles() {
 	for tag in \
