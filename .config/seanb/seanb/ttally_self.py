65a68d442a66e7718da1f813b16d82692a8680f6
Sean Breckenridge <seanbrecke@gmail.com>
2022-03-29 00:12:22 -0700

    add sreminder binding

--- .config/seanb/seanb/ttally_self.py
+++ .config/seanb/seanb/ttally_self.py
@@ -5,4 +5,4 @@ self_types_file = os.path.join(os.environ["HPIDATA"], "self_types.txt")
 with open(self_types_file, "r") as f:
     self_types = f.read().upper().splitlines()
 
-SelfTypes = Enum('SelfTypes', self_types)
+SelfTypes = Enum("SelfTypes", self_types)
