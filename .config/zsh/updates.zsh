2dd772a7b8173def75b54addc45b28eddd99261d
Sean Breckenridge <seanbrecke@gmail.com>
2022-03-29 16:18:50 -0700

    add update logs, update entr cache files

--- .config/zsh/updates.zsh
+++ .config/zsh/updates.zsh
@@ -2,10 +2,12 @@
 # move these into language specific functions, so I'm not updating everything all the time
 
 update_nvim() {
+	echo "Updating nvim..."
 	nvim +PlugUpdate +qall
 }
 
 update_npm() {
+	echo "Updating global npm packages..."
 	if command -v npm | grep -q 'npm-packages'; then
 		npm update -g
 	else
@@ -14,24 +16,29 @@ update_npm() {
 }
 
 update_gem() {
+	echo "Updating ruby packages..."
 	gem update
 } && alias update_ruby='update_gem'
 
 update_pip() {
+	echo "Updating python packages..."
 	python -m pip install --user -U -r "${XDG_CONFIG_HOME}/yadm/package_lists/python3_packages.txt"
 }
 
 update_cargo() {
+	echo "Updating cargo packages..."
 	cargo install-update -a
 }
 
 update_golang() {
+	echo "Updating golang packages..."
 	cd || return $?
 	spkglist "$HOME/.config/yadm/package_lists/go_packages.txt" | awk '{ print $2 "@latest" }' | xargs -I '{}' go install -v "{}"
 	cd "$OLDPWD" || return $?
 }
 
 update_basher() {
+	echo "Updating bash scripts..."
 	(cd "$(basher package-path .)" && git pull)
 	basher-upgrade-all
 }
