49fbacaf084f86c2b9d6644f0feff06a08fa3878
Sean Breckenridge <seanbrecke@gmail.com>
2022-11-20 19:50:43 -0800

    reorder ttally

--- .config/ttally.py
+++ .config/ttally.py
@@ -1,16 +1,7 @@
-# This defines some models for things I want to log often
-# which then generate into TUIs using:
 # https://github.com/seanbreckenridge/ttally
 
 from datetime import datetime
 from typing import NamedTuple
-
-from seanb.ttally_self import SelfTypes
-
-
-class Self(NamedTuple):
-    when: datetime
-    what: SelfTypes
 
 
 class Weight(NamedTuple):
@@ -18,27 +9,25 @@ class Weight(NamedTuple):
     pounds: float
 
 
-# this also tracks water, either by attaching it
-# to the corresponding food, or by just adding
-# something with the text 'water'
 class Food(NamedTuple):
     when: datetime
     calories: int
     food: str
     quantity: float
-    water: int  # ml
+    water: int  # how much ml of water was in this
 
-    # if I don't supply a quantity, default to 1
     @staticmethod
     def attr_validators() -> dict:
         # https://sean.fish/d/ttally_types.py?dark
         from seanb.ttally_types import prompt_float_default
 
+        # if I don't supply a quantity, default to 1
         return {"quantity": lambda: prompt_float_default("quantity")}
 
 
-# e.g. a concert or something
 class Event(NamedTuple):
+    """e.g. a concert or something"""
+
     event_type: str
     when: datetime
     description: str
@@ -47,7 +36,15 @@ class Event(NamedTuple):
 
     @staticmethod
     def attr_validators() -> dict:
-        # https://sean.fish/d/ttally_types.py?dark
+        # https://sean.fish/d/ttally_types.py?redirect
         from seanb.ttally_types import edit_in_vim
 
         return {"comments": edit_in_vim}
+
+
+from seanb.ttally_self import SelfTypes
+
+
+class Self(NamedTuple):
+    when: datetime
+    what: SelfTypes
