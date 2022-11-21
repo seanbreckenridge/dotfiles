329fff52de00a65e65667b8df34cb6d067fa5c49
Sean Breckenridge <seanbrecke@gmail.com>
2022-11-20 22:59:46 -0800

    add github issue id fzf function

--- .config/zsh/source_aliases.zsh
+++ .config/zsh/source_aliases.zsh
@@ -2,7 +2,7 @@ ALIAS_DIR="${ZDOTDIR}/aliases"
 source "${ALIAS_DIR}/aliases"         # General aliases
 source "${ALIAS_DIR}/git_aliases"     # Git aliases (from oh-my-zsh)
 source "${ALIAS_DIR}/project_aliases" # Aliases for my own projects
-source "${ALIAS_DIR}/lang_aliases"    # language/programming aliases
+source "${ALIAS_DIR}/dev_aliases"     # language tooling/programming aliases
 # Personal Aliases (e.g. ssh to servers)
 source_if_exists "${HPIDATA}/personal_aliases"
 # Tokens for interacting with APIs etc
