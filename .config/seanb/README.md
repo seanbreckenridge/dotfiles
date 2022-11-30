53932e96be1c69c0d0bbbb8b7ef441bb5313304b
Sean Breckenridge <seanbrecke@gmail.com>
2022-11-29 18:15:06 -0800

    seanb: convert to namespace package

--- .config/seanb/README.md
+++ .config/seanb/README.md
@@ -1,3 +1,3 @@
 This is installed as an package in `yadm bootstrap`
 
-Is essentially a package to store personal scripts/functions I want importable from anywhere
+package to store personal scripts/functions I want importable from anywhere
--- .config/seanb/setup.py
+++ .config/seanb/setup.py
@@ -1,13 +1,28 @@
-from setuptools import setup  # type: ignore[import]
+import os
+from pathlib import Path
+from setuptools import setup, find_namespace_packages
 
 
-if __name__ == "__main__":
+def main() -> None:
+    this_dir = Path(__file__).absolute().parent
+    if os.getcwd() != str(this_dir):
+        print(f"setuptools: cd'ing to {this_dir}")
+        os.chdir(this_dir)
+
     setup(
-        name="seanb",
-        zip_safe=False,
-        package_data={"seanb": ["py.typed"]},
-        packages=["seanb"],
+        name="seanb-personal",
+        packages=find_namespace_packages("src"),
+        url=f"https://github.com/seanbreckenridge/seanb-coreutils",
         author="Sean Breckenridge",
         author_email="seanbrecke@gmail.com",
         python_requires=">=3.8",
+        package_dir={"": "src"},
+        zip_safe=False,
+        description="seanb core utilities/functions",
+        license="MIT",
+        version="0.1.0",
     )
+
+
+if __name__ == "__main__":
+    main()
