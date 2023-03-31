de0565b5506f4cac21ed27737b48768fe7baa665
Sean Breckenridge <seanbrecke@gmail.com>
2023-03-30 19:04:26 -0700

    move filetype.vim to filetype.lua for .job detection

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -1,1 +1,9 @@
--- TODO: migrate to use this API instead of vimscript
+-- hmm I cant get this working
+-- vim.filetype.add {pattern = {[".*/.*%.job$"] = "bash"}}
+-- use autocmd for now...
+local ft_grp = vim.api.nvim_create_augroup("filetype_cmds", {clear = true})
+vim.api.nvim_create_autocmd({"BufEnter", "BufNewFile"}, {
+    group = ft_grp,
+    pattern = {"*.job", "*.job.disabled"},
+    callback = function(_) vim.cmd("set filetype=bash") end
+})
--- .config/nvim/ftdetect/filetype.vim
+++
@@ -1,5 +0,0 @@
-augroup bgproc_filetypes
-  autocmd!
-  " add filetype detection for bgproc - https://github.com/seanbreckenridge/bgproc
-  autocmd! BufRead,BufNewFile *.job,*.job.disabled set filetype=bash
-augroup END
