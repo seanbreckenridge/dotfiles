4f1e666b6caad6a94a3da78e4ca11d9444674b8b
Sean Breckenridge <seanbrecke@gmail.com>
2023-04-28 19:30:50 -0700

    nvim: add prisma tree-sitter

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -3,7 +3,7 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = {
         "bash", "cpp", "css", "html", "lua", "perl", "python", "rust", "go",
         "javascript", "json", "regex", "toml", "yaml", "vim", "typescript",
-        "tsx", "c", "java", "php", "ruby", "dart", "elixir", "query"
+        "tsx", "c", "java", "php", "ruby", "dart", "elixir", "query", "prisma"
     },
     highlight = {
         enable = true -- false will disable the whole extension
