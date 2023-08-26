ce97181b5b4611711bab1ccfa4e1db9c377319f8
Sean Breckenridge <seanbrecke@gmail.com>
2023-08-26 14:23:16 -0700

    add bgproc snippet

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -1,9 +1,9 @@
 -- hmm I cant get this working
--- vim.filetype.add {pattern = {[".*/.*%.job$"] = "bash"}}
+-- vim.filetype.add {pattern = {[".*/.*%.job$"] = "sh"}}
 -- use autocmd for now...
 local ft_grp = vim.api.nvim_create_augroup("filetype_cmds", {clear = true})
 vim.api.nvim_create_autocmd({"BufEnter", "BufNewFile"}, {
     group = ft_grp,
     pattern = {"*.job", "*.job.disabled"},
-    callback = function(_) vim.cmd("set filetype=bash") end
+    callback = function(_) vim.cmd("set filetype=sh") end
 })
