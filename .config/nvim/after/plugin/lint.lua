9b6cfad12343ada2229b346fab7718f9a0de1881
Sean Breckenridge <seanbrecke@gmail.com>
2023-08-29 19:55:00 -0700

    move autocmd for lint from vimscript to lua

--- .config/nvim/after/plugin/lint.lua
+++ .config/nvim/after/plugin/lint.lua
@@ -1,5 +1,15 @@
 -- https://github.com/mfussenegger/nvim-lint
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
+
 local flake8 = require 'lint.linters.flake8'
 flake8.cmd = 'flake8c'
 
-require'lint'.linters_by_ft = {python = {"mypy", "flake8"}}
+-- would add mypy here, but doesnt work great with imports
+-- pylint is good enough at that and typically faster
+
+require'lint'.linters_by_ft = {python = {"flake8"}}
--- .config/nvim/lua/seanbreckenridge/spelling.lua
+++ .config/nvim/lua/seanbreckenridge/spelling.lua
@@ -3,5 +3,4 @@ vim.cmd [[
 augroup seanbreckenridge_spelling
     autocmd!
     autocmd! BufRead,BufNewFile gitcommit | set spell
-    autocmd! BufWritePost lua require('lint').try_lint()
 augroup END]]
