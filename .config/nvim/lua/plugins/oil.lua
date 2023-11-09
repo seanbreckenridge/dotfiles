73350aba11178b4cff4e4778b4e4a04dcb3c840c
Sean Breckenridge <seanbrecke@gmail.com>
2023-11-09 13:21:41 -0800

    only enable trash-put if its available

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,9 +1,11 @@
+local has_trash_put = vim.fn.executable("trash-put") == 1
+
 return {
     "stevearc/oil.nvim",
     opts = {
         default_file_explorer = true,
         columns = {"icon"},
-        delete_to_trash = true,
+        delete_to_trash = has_trash_put,
         trash_command = "trash-put"
     }
 }
