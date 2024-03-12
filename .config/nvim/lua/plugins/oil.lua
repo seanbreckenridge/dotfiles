432280ef16b56b3ae9fabd168be9087e9193976a
Sean Breckenridge <seanbrecke@gmail.com>
2024-03-11 19:08:17 -0700

    nvim: remove check for trash-put
    oil implements the trash-put command, so we dont need
    to check if it exists of not

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,8 @@
-local has_trash_put = vim.fn.executable("trash-put") == 1
-
 return {
     "stevearc/oil.nvim",
     opts = {
         default_file_explorer = true,
         columns = {"icon"},
-        delete_to_trash = has_trash_put,
-        trash_command = "trash-put"
+        delete_to_trash = true,
     }
 }
