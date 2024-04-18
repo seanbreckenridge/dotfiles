12a2d0cde0700ea413addeb6a728c6ee76b27f76
Sean Breckenridge <seanbrecke@gmail.com>
2024-04-17 18:31:21 -0700

    nvim: temporarily disable treesitter context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
-        'nvim-treesitter/nvim-treesitter-context',
+        -- 'nvim-treesitter/nvim-treesitter-context',
         'nvim-treesitter/nvim-treesitter-textobjects',
         'JoosepAlviste/nvim-ts-context-commentstring', 'windwp/nvim-ts-autotag'
     },
