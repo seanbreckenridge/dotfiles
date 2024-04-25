31482864a0e98b22d21d3ef5aa9eb053b78d0f43
Sean Breckenridge <seanbrecke@gmail.com>
2024-04-25 12:45:02 -0700

    python: disable launching ipython, no tab completion support

--- .config/zsh/progressive_enhancement.zsh
+++ .config/zsh/progressive_enhancement.zsh
@@ -1,18 +1,14 @@
-# under certain (interactive, basic) conditions, upgrade a command
-# to a fancy version
-
-# if I type python with out any arguments, launch ipython instead
 python() {
 	python3 "$@"
 }
-python3() {
-	if (($# == 0)); then
-		echo -e "$(tput setaf 2)Launching ipython instead...$(tput sgr0)"
-		ipython
-	else
-		command python3 "$@"
-	fi
-}
+# python3() {
+# 	if (($# == 0)); then
+# 		echo -e "$(tput setaf 2)Launching ipython instead...$(tput sgr0)"
+# 		ipython
+# 	else
+# 		command python3 "$@"
+# 	fi
+# }
 
 man() {
 	if [[ -z "$1" ]]; then
