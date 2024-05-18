b0c9a0280e78210742fd8e058aa56f57ab859d04
Sean Breckenridge <seanbrecke@gmail.com>
2024-05-18 15:09:00 -0700

    nvim: remove default cloak config

--- .config/nvim/lua/plugins/markdown_preview.lua
+++ .config/nvim/lua/plugins/markdown_preview.lua
@@ -1,12 +1,9 @@
 return {
     "iamcco/markdown-preview.nvim",
     build = "cd app && yarn install",
-    -- only load when filetype is markdown
     ft = "markdown",
     config = function()
         local wk = require("which-key")
-        -- load plugin file so MarkdownPreview can be called
-
         wk.register({["p"] = {"<cmd>MarkdownPreview<cr>", "preview markdown"}},
                     {prefix = "<leader>"})
     end
