2be9b2032862801ce4b9aec5d5fbfaf61a335544
Sean Breckenridge <seanbrecke@gmail.com>
2024-05-21 01:56:54 -0700

    nvim: update completion plugins

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,19 +1,20 @@
+local wk = require('which-key')
+wk.register({
+    ["H"] = {
+        name = 'hex colors',
+        a = {'<cmd>ColorizerAttachToBuffer<CR>', 'attach'},
+        e = {'<cmd>ColorizerAttachToBuffer<CR>', 'enable'},
+        d = {'<cmd>ColorizerDetachFromBuffer<CR>', 'disable'}
+    }
+}, {prefix = '<leader>'})
+
 return {
     'norcalli/nvim-colorizer.lua',
-    event = "VeryLazy",
+    cmd = {'ColorizerAttachToBuffer', 'ColorizerDetachFromBuffer'},
+    keys = {'<leader>H'},
     config = function()
         -- https://github.com/norcalli/nvim-colorizer.lua
         -- colors hex codes/html colors etc #000000
-        local wk = require('which-key')
-        wk.register({
-            ["H"] = {
-                name = 'hex colors',
-                a = {'<cmd>ColorizerAttachToBuffer<CR>', 'attach'},
-                e = {'<cmd>ColorizerAttachToBuffer<CR>', 'enable'},
-                d = {'<cmd>ColorizerDetachFromBuffer<CR>', 'disable'}
-            }
-        }, {prefix = '<leader>'})
-
         require('colorizer').setup({
             '*', -- Highlight all files, but customize some others.
             css = {rgb_fn = true}, -- Enable parsing rgb(...) functions in css.
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    lazy = false,
+    event = {"BufRead", "BufNewFile"},
     dependencies = {
         'nvim-treesitter/nvim-treesitter-context',
         'nvim-treesitter/nvim-treesitter-textobjects', 'windwp/nvim-ts-autotag'
