bf9760c25dd9502fc62e1b029ccce95187497eaf
Sean Breckenridge <seanbrecke@gmail.com>
2024-05-22 11:29:35 -0700

    remove vim.keymap block

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -16,29 +16,23 @@ return {
 
         require("user.snippets")
 
-        -- Temporarily prevent this from erroring on older versions of neovim
-        -- I don't *need* to have snippets on my phone
-        if vim.keymap then
-            -- <c-k> is my expansion key
-            -- this will expand the current item or jump to the next item within the snippet.
-            vim.keymap.set({"i", "s"}, "<c-k>", function()
-                if ls.expand_or_jumpable() then
-                    ls.expand_or_jump()
-                end
-            end, {silent = true})
+        -- <c-k> is my expansion key
+        -- this will expand the current item or jump to the next item within the snippet.
+        vim.keymap.set({"i", "s"}, "<c-k>", function()
+            if ls.expand_or_jumpable() then ls.expand_or_jump() end
+        end, {silent = true})
 
-            -- <c-j> is my jump backwards key.
-            -- this always moves to the previous item within the snippet
-            vim.keymap.set({"i", "s"}, "<c-j>", function()
-                if ls.jumpable(-1) then ls.jump(-1) end
-            end, {silent = true})
+        -- <c-j> is my jump backwards key.
+        -- this always moves to the previous item within the snippet
+        vim.keymap.set({"i", "s"}, "<c-j>",
+                       function() if ls.jumpable(-1) then ls.jump(-1) end end,
+                       {silent = true})
 
-            -- <c-l> is selecting within a list of options.
-            -- This is useful for choice nodes
-            vim.keymap.set("i", "<c-l>", function()
-                if ls.choice_active() then ls.change_choice(1) end
-            end)
-        end
+        -- <c-l> is selecting within a list of options.
+        -- This is useful for choice nodes
+        vim.keymap.set("i", "<c-l>", function()
+            if ls.choice_active() then ls.change_choice(1) end
+        end)
 
         -- Extend with https://github.com/rafamadriz/friendly-snippets
         ls.filetype_extend("html", {})
