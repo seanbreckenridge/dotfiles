96026d751b2dea108fa252fe018faf3e1a485f67
Sean Breckenridge <seanbrecke@gmail.com>
2024-05-22 10:20:42 -0700

    nvim: autoformat

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -155,7 +155,7 @@ return {
                     override_generic_sorter = true, -- override the generic sorter
                     override_file_sorter = true, -- override the file sorter
                     case_mode = "smart_case" -- or "ignore_case" or "respect_case"
-                },
+                }
             }
         }
 
