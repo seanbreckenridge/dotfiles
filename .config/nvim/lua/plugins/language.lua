80995647ae722b15e60203ee13d1a4d629642ea8
Sean Breckenridge <seanbrecke@gmail.com>
2024-06-01 13:20:33 -0700

    nvim: skip updating lua workspace lib on android

--- .config/nvim/lua/plugins/language.lua
+++ .config/nvim/lua/plugins/language.lua
@@ -1,5 +1,5 @@
 return {
-    {'folke/lazydev.nvim', ft = 'lua'}, -- neovim/plugin docs/signatures
+    {'folke/lazydev.nvim', ft = 'lua', cond = not vim.g.on_android}, -- update lua workspace libraries
     {'jceb/vim-orgmode', ft = 'org', dependencies = {"tpope/vim-speeddating"}}, -- orgmode
     {'jxnblk/vim-mdx-js', ft = 'mdx'} -- extended markdown
 }
