51f09b26a5975908c5aa85df3bccfd4c1e24bc4e
Sean Breckenridge <seanbrecke@gmail.com>
2024-06-01 23:12:54 -0700

    nvim: remove embedded shlex.split highlight

--- .config/nvim/queries/python/injections.scm
+++ .config/nvim/queries/python/injections.scm
@@ -1,15 +1,3 @@
-;; highlight shell commands in shlex.split calls
-(call
- function: (attribute
-    object: (identifier) @py_object
-    attribute: (identifier) @py_function)
- (#eq? @py_object "shlex")
- (#eq? @py_function "split")
- arguments: (argument_list
-    (string (string_content) @injection.content))
- (#set! injection.language "bash")
- ) @python_highlight_shell_commands
-
 ;; highlight sqlite commands in python
 (call
  function: (attribute
