6102c635c0d169b09df100b2dd8086240827c499
Sean Breckenridge <seanbrecke@gmail.com>
2024-06-04 01:39:43 -0700

    nvim: use conform to format, lsp fallback

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -1,79 +1,85 @@
 local wk = require("which-key")
-wk.register({':write | Format<CR>', 'format'}, {prefix = '<leader>t'})
 
-local function cfg_format()
-    local util = require('formatter.util')
-    if util.get_current_buffer_file_name() == "setup.cfg" then
-        return {
-            exe = "setup-cfg-fmt-tempfile",
-            stdin = true,
-            args = {
-                util.escape_path(vim.fn.expand("%:p")),
-                "--include-version-classifiers"
-            }
-        }
-    else
-        return nil
-    end
-end
+wk.register({':Format<CR>', 'format'}, {prefix = '<leader>t'})
 
 return {
-    'mhartington/formatter.nvim',
-    -- hmm: using <leader>t as lazy load doesn't seem to work
+    'stevearc/conform.nvim',
     event = "VeryLazy",
-    cmd = {'Format', 'FormatWrite', 'FormatLock', 'FormatWriteLock'},
     config = function()
 
-        local fmt = require('formatter')
-        -- local util = require('formatter.util')
+        vim.api.nvim_create_user_command("Format", function(args)
+            local range = nil
+            if args.count ~= -1 then
+                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1,
+                                                            args.line2, true)[1]
+                range = {
+                    start = {args.line1, 0},
+                    ["end"] = {args.line2, end_line:len()}
+                }
+            end
+            require("conform").format({
+                async = true,
+                lsp_fallback = true,
+                range = range
+            }, function(err, did_edit)
+                if err ~= nil then return end
+                if did_edit then
+                    vim.notify("Format successful", vim.log.levels.INFO)
+                else
+                    vim.notify("No formatting needed", vim.log.levels.INFO)
+                end
+            end)
+        end, {range = true})
 
-        local prettier = require('formatter.defaults.prettier')
-        local shfmt = require('formatter.filetypes.sh').shfmt
-        local fixjson = require('formatter.filetypes.json').fixjson
-        local clang = require('formatter.defaults.clangformat')
-        local go = require('formatter.filetypes.go')
+        local prettier = {"prettier"}
 
-        -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-        fmt.setup {
-            -- Enable or disable logging
-            logging = true,
-            -- Set the log level
-            log_level = vim.log.levels.INFO,
-            -- All formatter configurations are opt-in
-            filetype = {
-                html = prettier,
-                json = fixjson,
-                typescript = prettier,
-                cfg = cfg_format,
+        require("conform").setup({
+            -- Map of filetype to formatters
+            formatters_by_ft = {
+                lua = {"lua-format"},
+                go = {"goimports", "gofmt"},
                 javascript = prettier,
-                typescriptreact = prettier,
-                javascriptreact = prettier,
-                css = prettier,
-                scss = prettier,
-                markdown = prettier,
+                typescript = prettier,
                 yaml = prettier,
                 toml = prettier,
-                php = prettier,
-                python = require('formatter.filetypes.python').black,
-                lua = require('formatter.filetypes.lua').luaformat,
-                rust = require('formatter.filetypes.rust').rustfmt,
-                go = {go.goimports, go.gofmt},
-                elixir = require('formatter.defaults.mixformat'),
-                sh = shfmt,
-                bash = shfmt,
-                c = clang,
-                cpp = clang,
-
-                -- Use the special "*" filetype for defining formatter configurations on
-                -- any filetype
-                ["*"] = {
-                    -- "formatter.filetypes.any" defines default configurations for any
-                    -- filetype
-                    {
-                        require("formatter.filetypes.any").remove_trailing_whitespace
-                    }
+                dosini = {"setup_cfg"}, -- often is misdetected
+                html = prettier,
+                json = {"fixjson"},
+                javascriptreact = prettier,
+                typescriptreact = prettier,
+                css = prettier,
+                elixir = {" mix"},
+                scss = prettier,
+                c = {"clang-format"},
+                cpp = {"clang-format"},
+                markdown = prettier,
+                python = {"black"},
+                sh = {"shfmt"},
+                bash = {"shfmt"},
+                -- run on all filetypes
+                ["*"] = {"codespell"},
+                ["_"] = {"trim_whitespace"}
+            },
+            -- Set the log level. Use `:ConformInfo` to see the location of the log file.
+            log_level = vim.log.levels.INFO,
+            -- Conform will notify you when a formatter errors
+            notify_on_error = true,
+            -- override the PWD environment variable to the directory of the setup.cfg
+            -- file. this is needed to discover stuff like the LICENSE/README
+            env = function() return {["PWD"] = vim.fn.expand('%:h')} end,
+            formatters = {
+                setup_cfg = {
+                    command = "setup-cfg-fmt-tempfile",
+                    stdin = true,
+                    -- cwd = require("conform.util").root_file({"setup.cfg"}),
+                    -- -- When cwd is not found, don't run the formatter (default false)
+                    -- require_cwd = true,
+                    -- When returns false, the formatter will not be used
+                    condition = function(_, ctx)
+                        return vim.fs.basename(ctx.filename) == "setup.cfg"
+                    end
                 }
             }
-        }
+        })
     end
 }
