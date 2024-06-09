5b6d66d042d270d2f5c8b721f3fa9b6de4dae925
Sean Breckenridge <seanbrecke@gmail.com>
2024-06-08 20:54:49 -0700

    nvim: update lint events

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -1,6 +1,6 @@
 return {
     'mfussenegger/nvim-lint',
-    event = 'BufRead',
+    event = {'BufReadPre', 'BufNewFile'},
     config = function()
         local codespell_enabled = not vim.g.on_android
         -- some custom wrapper commands that use my global config files
@@ -15,8 +15,13 @@ return {
 
         lint.linters_by_ft = {python = {"flake8"}}
 
-        -- run lint on save, or when lsp attaches
-        vim.api.nvim_create_autocmd({"BufWritePost", "LspAttach"}, {
+        -- Note: nvim-lint has an internal list of pre-enabled linters
+        -- you can disable by doing something like:
+        -- lint.linters_by_ft['json'] = nil
+
+        vim.api.nvim_create_autocmd({"BufEnter", "BufWritePost", "InsertLeave"},
+                                    {
+            group = vim.api.nvim_create_augroup("RunLinter", {clear = true}),
             callback = function()
                 lint.try_lint()
                 if codespell_enabled then
