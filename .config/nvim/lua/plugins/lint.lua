5f82e904e3c50c82471260a1e7b265a155a96db5
Sean Breckenridge <seanbrecke@gmail.com>
2024-06-08 19:47:45 -0700

    nvim: optionally enable codespell using nvim-lint

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -2,17 +2,27 @@ return {
     'mfussenegger/nvim-lint',
     event = 'BufRead',
     config = function()
-        local flake8 = require 'lint.linters.flake8'
-        flake8.cmd = 'flake8c'
+        local codespell_enabled = not vim.g.on_android
+        -- some custom wrapper commands that use my global config files
+        require('lint.linters.flake8').cmd = 'flake8c'
+        if codespell_enabled then
+            require('lint.linters.codespell').cmd = 'codespell-conf'
+        end
 
         -- would add mypy here, but doesn't work great with imports
         -- pyright is good enough at that and typically faster
+        local lint = require('lint')
 
-        require'lint'.linters_by_ft = {python = {"flake8"}}
+        lint.linters_by_ft = {python = {"flake8"}}
 
         -- run lint on save, or when lsp attaches
         vim.api.nvim_create_autocmd({"BufWritePost", "LspAttach"}, {
-            callback = function() require("lint").try_lint() end
+            callback = function()
+                lint.try_lint()
+                if codespell_enabled then
+                    lint.try_lint('codespell')
+                end
+            end
         })
     end
 }
