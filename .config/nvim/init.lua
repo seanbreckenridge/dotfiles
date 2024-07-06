ba772164cc6c207892f63be25f99e1140ba8a0c0
Sean Breckenridge <seanbrecke@gmail.com>
2024-07-06 14:20:54 -0700

    nvim: remove vim.loop fallback

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
