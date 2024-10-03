b6f3a545da8f917414f48cfeb9795b9477ec98a0
Sean Breckenridge <seanbrecke@gmail.com>
2024-10-02 21:24:24 -0700

    follow links when CD

--- .config/zsh/cd.zsh
+++ .config/zsh/cd.zsh
@@ -16,7 +16,7 @@ bindkey '^[l' fzf-cd-widget
 CD() {
 	local chosen
 	cd ~
-	chosen="$(fd --type d | fzf --no-multi --height=10 --reverse --preview="tree -C {}")"
+	chosen="$(fd -L --type d | fzf --no-multi --height=10 --reverse --preview="tree -C {}")"
 	[[ -z "$chosen" ]] && return 1
 	cd "$chosen" || return $?
 }
